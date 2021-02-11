e16da51a9758b652f6a3b2fa0d04fbebca9cb18c
Julien Mathieu <julienmathieu.contact@gmail.com>
2021-02-11 14:30:05 +0100

    finished models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cocktails_serialized = open(url).read
+cocktails = JSON.parse(cocktails_serialized)
+
+cocktail_name = cocktails["drinks"][1]["strIngredient1"]
+
+cocktails.each do |drink|
+  cocktail_name
+end
