c928d1642169e3bc32427cf8c9dda079b7fdf672
Julien Mathieu <julienmathieu.contact@gmail.com>
2021-02-12 12:15:19 +0100

    A bit of front

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Ingredient.destroy_all
+
 Dose.destroy_all
+Ingredient.destroy_all
 Cocktail.destroy_all
 
 require 'json'
